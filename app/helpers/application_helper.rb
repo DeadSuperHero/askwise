3b4efb9b033066c38d2c4f239e25faeafd3a80cb
DeadSuperHero <sean@deadsuperhero.com>
2017-07-29 16:10:38 -0700

    initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+
+  def avatar_for(user, options = { size:50})
+   	avatar_id = Digest::MD5::hexdigest(current_user.email).downcase
+   	"https://seccdn.libravatar.org/avatar/#{avatar_id}.png?s=30"
+   end
+
+end
