1cdcbe481d371da0e634430e5f67e570708d5a8b
DeadSuperHero <sean@deadsuperhero.com>
2017-07-30 15:14:24 -0700

    fix libravatar matching issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def avatar_for(user, options = { size:50})
-   	avatar_id = Digest::MD5::hexdigest(current_user.email).downcase
+   	avatar_id = Digest::MD5::hexdigest(user.email).downcase
    	"https://seccdn.libravatar.org/avatar/#{avatar_id}.png?s=30"
    end
 
