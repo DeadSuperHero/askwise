cab9676f4c0a2f3b8780f954c1b56741bee2fde3
DeadSuperHero <sean@deadsuperhero.com>
2017-07-30 14:59:10 -0700

    fix redirect logic

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  before_action :find_question
   before_action :find_answer
   before_action :authenticate_user!
 
@@ -32,7 +32,7 @@ class AnswersController < ApplicationController
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.html { redirect_to @question, notice: 'Answer was successfully created.' }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new }
@@ -58,13 +58,13 @@ class AnswersController < ApplicationController
   # DELETE /answers/1
   # DELETE /answers/1.json
   def destroy
-    @answer = @answer.answers.find(params[:id])
+    @answer = @question.answers.find(params[:id])
 
     if @answer.user == current_user
       @answer.delete
 
     respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
+      format.html { redirect_to @question, notice: 'Answer was successfully destroyed.' }
       format.json { head :no_content }
     end
     end
