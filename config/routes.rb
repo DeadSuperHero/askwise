347ab56144f0506b66b99bcdfae97c4245e48b9e
DeadSuperHero <sean@deadsuperhero.com>
2017-07-31 09:24:19 -0700

    add routing support for tags, needs more work

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+    resources :tags, only: [:index, :show]
+
     authenticated :user do
       root :to => 'questions#index', as: :authenticated
     end
