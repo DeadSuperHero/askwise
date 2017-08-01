03a49520c6f586d96744384ea1ae969b72e0e1b1
DeadSuperHero <sean@deadsuperhero.com>
2017-08-01 01:05:03 -0700

    cosmetic fixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,10 @@ class Question < ApplicationRecord
   has_many :answers
   has_many :tags
   acts_as_taggable_on :tags
+
+  def similar
+    question_ary = Question.tagged_with(self.tags, :any => true)
+    question_ary.uniq.delete(self)
+  end
+
 end
