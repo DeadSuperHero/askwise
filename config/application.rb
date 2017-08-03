10772777d7d6c881f3b3de7f3b8f8779776efe49
DeadSuperHero <sean@deadsuperhero.com>
2017-08-03 10:44:18 -0700

    try again

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Askwise
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    config.assets.digest = true
 
   end
 end
