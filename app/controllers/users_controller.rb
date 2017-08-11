8479912615a1f14366a69dc0afe3015d6b196125
Sean Tilley <sean@localhost.localdomain>
2017-08-10 20:02:33 -0700

    FIXED

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
 
    def show
      @user = User.find(params[:id])
-     @user = User.includes(:questions, :answers)
    end
 
 
    private
+
    def user_params
-     params.require(:user).permit(:email)
+     params.require(:user).permit(:email, :id)
    end
 
  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     get 'tags/:tag', to:'questions#index', as: :tag
-    get 'users/:id' =>  'users#show'
 
     authenticated :user do
       root :to => 'questions#index', as: :authenticated
     end
 
     root :to=> 'page#home'
+
+     resources :users, only:[:show] do
+     end
 
   resources :questions do
     resources :answers
