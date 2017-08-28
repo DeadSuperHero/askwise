adabeb650ed24a76b69e9bd7b2e74d0be80356a7
deadsuperhero <sean@deadsuperhero.com>
2017-08-28 15:07:52 -0700

    generate basic fixtures for admin, dashboard, projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :dashboards
+  resources :projects
+  devise_for :admins
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
